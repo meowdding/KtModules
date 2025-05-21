260d4c7f3787db3bf5bb6720ba5d3dc205d95d3f
Mona <59416038+meowora@users.noreply.github.com>
2025-05-21 17:21:33 +0200

    feat: create java 8 publications for empa :3

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,7 +1,13 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.KotlinApiPlugin
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.1.20"
     `maven-publish`
 }
+
+apply<KotlinApiPlugin>()
 
 repositories {
     maven(url = "https://maven.teamresourceful.com/repository/maven-public/")
@@ -22,16 +28,46 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
+    target {
+        compilations.create("java8") {
+            defaultSourceSet.kotlin.srcDir(project.sourceSets.main.get().kotlin.srcDirs)
+            defaultSourceSet.resources.srcDir(project.sourceSets.main.get().resources.srcDirs)
+            source(project.sourceSets.main.get())
+            defaultSourceSet {
+                dependencies {
+                    for (file in project.sourceSets.main.get().compileClasspath.files) {
+                        implementation(files(file))
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.getByName<KotlinCompile>("compileJava8Kotlin") {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
+}
+
+tasks.getByName<JavaCompile>("compileJava8Java") {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
+val java8Jar = tasks.create("java8jar", Jar::class) {
+    mustRunAfter(tasks.getByName("java8Classes"))
+    from(sourceSets.getByName("java8").output)
+    archiveClassifier.set("java-8")
 }
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        fun createDefault(name: String, task: Jar, suffix: String = "") = create<MavenPublication>(name) {
             artifactId = "KtModules"
-            from(components["java"])
+            artifact(task)
+            version = project.version.toString() + suffix
 
             pom {
-                name.set("KtModules")
+                this.name.set("KtModules")
                 url.set("https://github.com/meowdding/ktmodules")
 
                 scm {
@@ -41,6 +77,8 @@ publishing {
                 }
             }
         }
+        createDefault("maven", tasks.jar.get())
+        createDefault("java8", java8Jar, "+java-8")
     }
     repositories {
         maven {
